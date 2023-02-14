4104db4accedf2d983f7dccbcb0382132f8c8667
Achinov <gabrielarquilinoa.a@gmail.com>
2023-02-14 12:14:53 -0300

    novo save

---
+++ aula1/exemplo1.php
@@ -0,0 +1,13 @@
+<?php
+    $ola= "Olá mundo";
+    echo gettype($ola);
+    echo "<br>";
+    echo $ola;
+
+    $ola=10;
+    echo "<br>";
+    echo gettype($ola);
+    echo "<br>";
+    echo $ola;
+
+?>
