ff51a577f597da074b3cff1d89aa4494c4b858a3
Achinov <gabriel.arquilino@aluno.ifsp.ed.br>
2023-03-07 10:07:53 -0300

    boa

---
+++ lista1/ex3.php
@@ -0,0 +1,10 @@
+<?php
+
+$R = readline ();
+$n=3.14159;
+$A= ($R*$R)*$n;
+
+$A = number_format($A, 4, ".", "");
+echo "A=$A\n";
+
+?>
---
+++ lista1/ex4.php
@@ -0,0 +1,11 @@
+<?php
+
+$X = readline ();
+$Y = readline ();
+
+$PROD = $X*$Y;
+
+echo "PROD = ".$PROD;
+echo "\n";
+
+?>
