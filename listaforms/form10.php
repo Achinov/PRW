3131527e4ec9fa4f42fac162fdada855896ec2a3
Achinov <gabrielarquilinoa.a@gmail.com>
2023-05-02 09:23:22 -0300

    cassio

--- listaforms/form10.php
+++ listaforms/form10.php
@@ -1,15 +1,6 @@
 <?php
 
-$nome= $_POST['nome'];
-$horario= $_POST['horario'];
-if($horario>18){ 
-    echo "<h1>Boa noite $nome</h1>";
-}
-if($horario<12){ 
-    echo "<h1>Bom dia $nome</h1>";
-}
-else{ 
-    echo "<h1>Boa tarde $nome</h1>";
-}
+$t= ($_POST['P']*10) + ($_POST['M']*12) + $_POST['G']*15;
+ echo "Total arrecadado: $t\n";
 
 ?>
